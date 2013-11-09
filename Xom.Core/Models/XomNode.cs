3affd0cc8e5701c8733471ff600a8a041ba91cde
KallDrexx <me@mshapiro.net>
2013-11-08 19:22:37 -0500

    Updated XomNode type to provide a useful ToString() overload

--- Xom.Core/Models/XomNode.cs
+++ Xom.Core/Models/XomNode.cs
@@ -8,6 +8,15 @@ namespace Xom.Core.Models
         public Type Type { get; set; }
         public bool IsRoot { get; set; }
         public IEnumerable<XomNodeChild> Children { get; set; }
-        public IEnumerable<XomNodeAttribute> Attributes { get; set; } 
+        public IEnumerable<XomNodeAttribute> Attributes { get; set; }
+
+        public override string ToString()
+        {
+            string typeName = Type != null
+                                  ? Type.Name
+                                  : "null";
+
+            return "XomNode: " + typeName;
+        }
     }
 }
